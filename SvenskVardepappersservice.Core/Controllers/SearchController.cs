58cc4c8ecb3e96265c75c401117d32f4390f7445
Ester Sandstrom <sandstrom.ester@gmail.com>
2021-06-23 12:53:39 +0200

    Search for documents on page

---
+++ SvenskVardepappersservice.Core/Controllers/SearchController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Services;
+using System.Web.Mvc;
+using Examine;
+using Igloo.Core;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Examine;
+using Umbraco.Web;
+using Umbraco.Web.WebApi;
+
+namespace SvenskVardepappersservice.Core.Controllers
+{
+    public class SearchResult
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string CreateDate { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+    }
+
+    public class SearchResponse
+    {
+        public List<SearchResult> Result { get; set; }
+        public long ResultCount { get; set; }
+    }
+
+    public class SearchController : UmbracoApiController
+    {
+        ILocalizationService _localizationService = null;
+        IglooSettings _settings = null;
+
+        public SearchController(ILocalizationService localizationService)
+        {
+            _localizationService = localizationService;
+            _settings = new IglooConfig().Settings;
+        }
+
+        [HttpGet]
+        public SearchResponse GetSearch(string query, int amount, int page)
+        {
+            var culture = "";
+            var searchResults = new SearchResponse() { Result = new List<SearchResult>(), ResultCount = 0 };
+            if (!ExamineManager.Instance.TryGetIndex(Constants.UmbracoIndexes.ExternalIndexName, out var index) || !(index is IUmbracoIndex umbIndex))
+                throw new InvalidOperationException($"The required index {Constants.UmbracoIndexes.ExternalIndexName} was not found");
+
+            var documentTypeAliases = new[] { "document" };
+            var fieldAliases = new[] {"uploadFile", "name", "description"};
+
+            var fields = new string[]{};
+
+            foreach (var fieldAlias in fieldAliases) {
+                var alias = string.IsNullOrEmpty(culture) ? fieldAlias : fieldAlias+"_" + culture.ToLower();
+                fields = fields.Append(alias).ToArray();
+            }
+
+            var isPublished = string.IsNullOrEmpty(culture) ? "__Published" : "__Published_" + culture.ToLower();
+            var searcher = index.GetSearcher();
+
+            var examineQuery = searcher
+                // Only search content
+                .CreateQuery("content")
+                // Managed query for search term. A managed query means it's up to the field definition
+                // to build the appropriate query (i.e. full text, wildcard, phrase, etc...)
+                // nodeName will be indexed as 'fulltext' type which resolves to 
+                // https://github.com/Shazwazza/Examine/blob/master/src/Examine/LuceneEngine/Indexing/FullTextType.cs#L69
+                .ManagedQuery(query, fields)
+                // Only published items
+                .And().Field(isPublished, "y")
+                // Only these content types
+                .And().GroupedOr(new[] { "__NodeTypeAlias" }, documentTypeAliases);
+            examineQuery = examineQuery
+                // Exclude anything hidden from search, NOT filters must come last!
+                .Not().Field("hideFromSearch", "1");
+            var skip = (page - 1) * amount;
+            var take = amount;
+     
+            var results = Umbraco.ContentQuery.Search(examineQuery, skip, take, out long totalRecords);
+            searchResults.ResultCount = totalRecords;
+            foreach (var result in results)
+            {
+                var node = result.Content;
+                var fileUrl = node.Value<string>("UploadFile");
+                searchResults.Result.Add(new SearchResult() { Name = node.Name, Url = fileUrl, CreateDate =  node.CreateDate.ToString("d MMMM yyyy", new System.Globalization.CultureInfo("sv-SE")), FileType = node.UrlSegment, Description = node.Value<string>("Description"), FileName = fileUrl.Substring(fileUrl.LastIndexOf('/') + 1)});
+            }
+            return searchResults;
+        }
+    }
+}
---
+++ SvenskVardepappersservice.Web/Scripts/app.js
@@ -0,0 +1,103 @@
+var listItems = document.querySelectorAll(".list-container");
+listItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+        window.location = item.querySelector(".button").href;
+    });
+});
+
+var mobileNav = document.querySelector(".mobile-nav");
+
+mobileNav.addEventListener("click", function (e) {
+    e.preventDefault();
+    mobileNav.classList.toggle("mobile-nav-open");
+});
+/*======================================
+  #Full Search
+======================================*/
+(function () {
+    var searchInput = document.querySelector(".search-block .search__form .search__input");
+    var searchresult = document.querySelector(".searchresult"); //searchresults
+    var pager = document.querySelector(".search-pagination-container button");
+    if (searchInput && searchresult) {
+        //Preform search on page load
+        doFullSearch(searchInput.value, 1);
+        //Preform search on input change
+        searchInput.addEventListener("input", function (e) {
+            doFullSearch(searchInput.value, 1);
+            document.querySelector(".query").innerHTML = '"' + searchInput.value + '"';
+        });
+        //pager
+        pager.addEventListener("click", function (e) {
+            pager.dataset.currentPage = (pager.dataset.currentPage ? parseInt(pager.dataset.currentPage) + 1 : 2).toString();
+            doFullSearch(searchInput.value, parseInt(pager.dataset.currentPage));
+        });
+    }
+})();
+function doFullSearch(query, page) {
+    // var resultLabelSingle = document.querySelector(".search-results-count-label.single");
+    // var resultLabelMultible = document.querySelector(".search-results-count-label.multi");
+    // var resultLabelCount = document.querySelector(".search-results-count");
+    // var resultLabelQuery = document.querySelector(".search-result-info strong.query");
+    var searchEmpty = document.querySelector(".search-empty");
+    var pager = document.querySelector(".search-pagination-container");
+    var amountPerPageElement = document.querySelector(".search-pagination-amount");
+    var amountPerPage = 10 // parseInt(amountPerPageElement.value);
+    axios({
+        url: "/Umbraco/Api/Search/GetSearch?Query=" + query + "&Amount=" + amountPerPage + "&Page=" + page,
+        method: "GET"
+    }).then(function (response) {
+        // if (response.data.ResultCount == 1) {
+        //     resultLabelSingle.style.display = 'inline-block';
+        //     resultLabelMultible.style.display = 'none';
+        // }
+        // else {
+        //     resultLabelMultible.style.display = 'inline-block';
+        //     resultLabelSingle.style.display = 'none';
+        // }
+        // resultLabelCount.textContent = '"' + query + '"';
+        // resultLabelCount.textContent = response.data.ResultCount;
+        // No results
+        if (response.data.ResultCount == 0) {
+            searchEmpty.style.display = 'block';
+            document.querySelectorAll('.search-result, .search-result-container').forEach(function (e) { return e.remove(); });
+            pager.style.display = 'none';
+        }
+        else {
+            searchEmpty.style.display = 'none';
+            if (page == 1) {
+                document.querySelectorAll('.search-result, .search-result-container').forEach(function (e) { return e.remove(); });
+            }
+            var template = document.querySelector(".search-results template");
+            for (var i = 0; i < response.data.Result.length; i++) {
+                var clone = template.content.cloneNode(true);
+                var fileUrl = response.data.Result[i].Url;
+                var fileType = response.data.Result[i].FileType;
+                var src = ""
+                if (fileType === "doc" || fileType === "pdf" || fileType === "pptx" || fileType === "xlsx")
+                {
+                    src = "/assets/Icons/file-" + fileType + ".svg";
+                }
+                else if (fileUrl !== "")
+                {
+                    src = "/assets/Icons/paper-2.svg"
+                }  
+                
+                clone.querySelectorAll("a").forEach(function (e) { return e.setAttribute("href", fileUrl); });
+                clone.querySelector("h2").textContent = response.data.Result[i].Name;
+                clone.querySelector(".pre-head").textContent = response.data.Result[i].CreateDate;
+                clone.querySelector(".info-popup").textContent = response.data.Result[i].FileName;
+                clone.querySelector("img").setAttribute("src", src);
+                clone.querySelector("p").textContent = response.data.Result[i].Description;
+                template.parentElement.appendChild(clone);
+            }
+            if (document.querySelectorAll(".search-result, .search-result-container").length < response.data.ResultCount) {
+                pager.style.display = 'block';
+            }
+            else {
+                pager.style.display = 'none';
+            }
+        }
+        resultLabelCount.innerHTML = response.data.ResultCount;
+    })["catch"](function () {
+    });
+}
