e72e0adac62f89ce7ceaf2af7fa4d17ae2b2daa9
Erik <erik@webmind.se>
2021-07-20 14:12:42 +0200

    Changed path of login to custom path and restructure

---
+++ SvenskVardepappersservice.Core/Features/Composer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace SvenskVardepappersservice.Core.Features
+{
+
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class ExternalLoginComposer : IUserComposer
+    {
+
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<LoginRouteComponent>();
+        }
+    }
+
+    public class LoginRouteComponent : IComponent
+    {
+        public void Initialize()
+        {
+            RouteTable.Routes.MapRoute(
+                name: "ExternalLogin",
+                url: "bwlogin",
+                defaults: new { controller = "ExternalLogin", action = "Login" }
+            );
+        }
+
+        public void Terminate()
+        {
+            // Do nothing
+        }
+    }
+   
+}
